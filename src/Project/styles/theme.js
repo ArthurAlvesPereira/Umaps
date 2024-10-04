df35aebea9ca6e5785454afc69f5d512f995ab26
Arthur Pereira <arthurap2001@gmail.com>
2024-10-04 12:37:06 -0300

    assets: Adcionado arquivo de temas e vetores

---
+++ src/Project/styles/theme.js
@@ -0,0 +1,16 @@
+export const colors = {
+    green: '#147B40',
+    orange: '#E2650E',
+    black: '#161513',
+    grey: '#454647',
+    white: '#F5F6F3',
+}
+
+export const fonts = {
+    //fire sans condensed
+
+    regular: 'FireSansCondensed-Regular',
+    medium: 'FireSansCondensed-Medium',
+    bold: 'FireSansCondensed-Bold',
+    black: 'FireSansCondensed-Black',
+}
